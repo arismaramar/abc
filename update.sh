eeb487c3e1bfec33e5a48873a020dd4bf634c08f
free net <74614880+arismaramar@users.noreply.github.com>
2023-04-20 10:42:55 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-REPO="https://raw.githubusercontent.com/myridwan/abc/ipuk/"
+REPO="https://raw.githubusercontent.com/arismaramar/abc/main/"
 echo -e " [INFO] Downloading Update File" | lolcat
 sleep 2
 rm -rf /tmp/menu
